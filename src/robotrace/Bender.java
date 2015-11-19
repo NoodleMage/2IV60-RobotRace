235d88ac5670ed2e12ca39cb1804dceeb64abce9
Indi Nijhof <indinijhof@gmail.com>
2015-11-20 00:29:55 +0100

    Bender torso and head in progress

--- src/robotrace/Bender.java
+++ src/robotrace/Bender.java
@@ -7,6 +7,8 @@ import static javax.media.opengl.fixedfunc.GLLightingFunc.GL_DIFFUSE;
 import static javax.media.opengl.fixedfunc.GLLightingFunc.GL_SHININESS;
 import static javax.media.opengl.fixedfunc.GLLightingFunc.GL_SPECULAR;
 import javax.media.opengl.glu.GLU;
+import javax.media.opengl.glu.*;
+//import javax.media.opengl.glu.GLUquadric;
 
 /**
 * Represents a Robot, to be implemented according to the Assignments.
@@ -27,7 +29,14 @@ class Bender {
     private GLU glu;
     private GLUT glut;
     // Set the Robot stats
+    private final int stacks = 30;
+    private final int slices = 30;
     private boolean stick;
+    private final double scale = 0.1;
+    private double bodyWidth;
+    private double bodyWidthRadius;
+    private double bodyHeight;
+    
 
     /**
      * Constructs the robot with initial parameters.
@@ -47,11 +56,52 @@ class Bender {
         this.glut = glut1;
         this.stick = stickFigure; 
         
-       //Draw Robot
-       drawBody(tAnim);
-       drawLegs(tAnim);
-       drawArms(tAnim);
-       drawHead(tAnim);
+        // Set the base values fot the bot
+        bodyWidth = 9;
+        bodyWidthRadius = bodyWidth/2;
+        bodyHeight = 11;
+        
+        // Draw the bot
+        gl.glPushMatrix();
+        // Apply the universal scaling of the bot
+        gl.glScaled(scale, scale, scale);
+        
+        drawBody(tAnim);
+        drawLegs(tAnim);
+        drawArms(tAnim);
+        drawHead(tAnim);
+       
+       gl.glPopMatrix();
+    }
+    
+    /***
+     * Draws the robot head
+     */
+    public void drawHead(float tAnim){
+        gl.glPushMatrix();
+        // set the dimensions
+        double headWidth = (bodyWidth/11)*5;
+        double faceHeight = headWidth * 1.5;
+        double foreheadRadius = headWidth/2;
+        double antennaBaseRadius = foreheadRadius/5;
+        // Draw the face
+        gl.glTranslated(0, 0, bodyHeight);
+        glut.glutSolidCylinder(foreheadRadius, faceHeight, slices, stacks);
+        // Draw the forehead
+        gl.glTranslated(0, 0, faceHeight);
+        glut.glutSolidSphere(foreheadRadius, slices, stacks);
+        //Draw antenna-base
+        gl.glTranslated(0, 0, foreheadRadius);
+        glut.glutSolidCone(antennaBaseRadius*0.75, antennaBaseRadius*6, slices, stacks);
+        glut.glutSolidSphere(antennaBaseRadius, slices, stacks);
+        gl.glTranslated(0, 0, antennaBaseRadius*5);
+        glut.glutSolidSphere(antennaBaseRadius/2, slices, stacks);
+        //test
+        gl.glTranslated(0, foreheadRadius*1.5, -faceHeight);
+        gl.glRotated(90, 1, 0, 0);
+        gl.glScaled(1, 0.5, 0.5);
+        glut.glutSolidCylinder(foreheadRadius, faceHeight, slices, stacks);
+        gl.glPopMatrix();
     }
     
     /**
@@ -59,7 +109,15 @@ class Bender {
      */
     public void drawBody(float tAnim){
         gl.glPushMatrix();
-    
+        //GLUquadric test = glu.gluNewQuadric();
+        //glu.gluCylinder(test, 6.5, 9, 11, 30, 30);
+        //glu.gluDeleteQuadric(test);
+        
+        // Draw the body
+        glut.glutSolidCylinder(bodyWidthRadius, bodyHeight, slices, stacks);
+        gl.glTranslated(0, 0, bodyHeight);
+        // Draw the "neck"
+        glut.glutSolidCone(bodyWidthRadius, bodyHeight*0.3, slices, stacks);
         gl.glPopMatrix();
     }
     
@@ -84,15 +142,6 @@ class Bender {
     public void drawLegs(float tAnim){
         gl.glPushMatrix();
 
-        gl.glPopMatrix();
-    }
-    
-    /***
-     * Draws the robot head
-     */
-    public void drawHead(float tAnim){
-        gl.glPushMatrix();
-        
         gl.glPopMatrix();
     }
 }
